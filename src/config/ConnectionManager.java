b1009258648600440e6f5c4aa319fdf46176aaa9
Matroskeen <likebluebanana@gmail.com>
2015-05-18 11:02:25 +0300

    Added config_project.ini

--- src/config/ConnectionManager.java
+++ src/config/ConnectionManager.java
@@ -1,19 +1,47 @@
 package config;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.*;
+
+import org.ini4j.Ini;
+import org.ini4j.InvalidFileFormatException;
 
 public class ConnectionManager {
 
 	static Connection con;
 	static String url;
+	static String admin;
+	static String password;
 
 	public static Connection getConnection() {
+		
+		try {
+			String filePath = "D:\\Projects\\Eclipse\\StudentsTestProject\\config_project.ini";
+			
+			Ini ini = new Ini(new FileReader(filePath));
+			Ini.Section config = ini.get("database");
 
-		String url = "jdbc:mysql://localhost:3306/ourproject";
+	        url = config.get("url");
+	        admin = config.get("admin");
+	        password = config.get("password");
+			
+			
+		} catch (InvalidFileFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver"); 
-			con = DriverManager.getConnection(url, "admin", "admin");
+			con = DriverManager.getConnection(url, admin, password);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
