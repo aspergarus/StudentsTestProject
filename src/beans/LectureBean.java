f54770e7831241c04965d23c14c248e3a0fb9a62
Matroskeen <likebluebanana@gmail.com>
2015-05-21 11:54:00 +0300

    Added bean, dao, servlet for lectures

---
+++ src/beans/LectureBean.java
@@ -0,0 +1,47 @@
+package beans;
+
+public class LectureBean {
+	private int id;
+	private int idTeacher;
+	private int size;
+	private String title;
+	private String fileName;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIdTeacher() {
+		return idTeacher;
+	}
+	public void setIdTeacher(int idTeacher) {
+		this.idTeacher = idTeacher;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/LecturesServlet.java
@@ -0,0 +1,50 @@
+package main;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.LectureBean;
+import beans.UserBean;
+import dao.LectureDAO;
+
+/**
+ * Servlet implementation class LecturesServlet
+ */
+@WebServlet("/lectures")
+public class LecturesServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LecturesServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+		UserBean user = (session != null) ? (UserBean) session.getAttribute("user") : null;
+		if (user == null || user.getRole() != 2) {
+			request.setAttribute("message", "You don't have access to this page.");
+			request.getRequestDispatcher("error-access.jsp").forward(request, response);
+		}
+		else {
+			
+			ArrayList<LectureBean> lectures = LectureDAO.findAll(user);
+			request.setAttribute("lecturesList", lectures);
+			request.getRequestDispatcher("lectures.jsp").forward(request, response);
+		}
+	}
+
+}
